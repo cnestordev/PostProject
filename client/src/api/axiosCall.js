1ed33b50c3c880cdaf09d5bd5f74eb32bd78348a
Nestor <ncervan87@gmail.com>
2021-03-08 14:20:35 -0800

    updates axiosCall url

--- client/src/api/axiosCall.js
+++ client/src/api/axiosCall.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 // macbook url
 const macUrl = 'http://192.168.1.14:3001'
 const localUrl = 'http://localhost:3001'
+const herokuUrl = 'https://memeit-backend.herokuapp.com/'
 
 const axiosCall = axios.create({
-  baseURL: macUrl,
+  baseURL: herokuUrl,
   withCredentials: true,
 })
 
