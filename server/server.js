59c9c8d08aef40a926d53b41de179b75e46aa170
Nestor <ncervan87@gmail.com>
2021-03-08 12:30:18 -0800

    makes changes to cors options

--- server/server.js
+++ server/server.js
@@ -69,6 +69,17 @@ store.on('error', err => {
   console.log(err)
 })
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://memeit-client.vercel.app')
+  res.header('Access-Control-Allow-Headers', true)
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+  next()
+})
+
 app.use(
   session({
     store: MongoStore.create({
