3d90e7de2c80ae20ef94aeaa89bd4d69f44c5a7a
Nestor <ncervan87@gmail.com>
2021-03-08 14:19:13 -0800

    modifies axiosCall

--- client/src/api/axiosCall.js
+++ client/src/api/axiosCall.js
@@ -5,7 +5,8 @@ const macUrl = 'http://192.168.1.14:3001'
 const localUrl = 'http://localhost:3001'
 
 const axiosCall = axios.create({
-  baseURL: localUrl,
+  baseURL: macUrl,
+  withCredentials: true,
 })
 
 export default axiosCall
