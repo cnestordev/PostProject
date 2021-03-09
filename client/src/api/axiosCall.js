c46d70dc4df8850510934b489c6113b01007bb0e
Nestor <ncervan87@gmail.com>
2021-03-08 23:04:11 -0800

    updated seeds

--- client/src/api/axiosCall.js
+++ client/src/api/axiosCall.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-// macbook url
+// macUrl
 const macUrl = 'http://192.168.1.14:3001'
 const localUrl = 'http://localhost:3001'
 const herokuUrl = 'https://memeit-backend.herokuapp.com'
