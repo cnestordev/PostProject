eb23ebb7eb2f3260d7c44c01a773e7a80854e0a5
Nestor Cervantes <65986735+cnestordev@users.noreply.github.com>
2022-04-11 10:37:28 -0700

    Revert "some ui updates"

--- client/src/styles/popup.js
+++ client/src/styles/popup.js
@@ -26,14 +26,13 @@ export const Box = styled.div`
   animation-iteration-count: 1;
   animation: ${fadeOut} 1s ease-out 4.5s;
   background: white;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 3px 0px;
   position: fixed;
   right: 2%;
   width: 15%;
   transition: 200ms;
   padding: 0.65%;
   text-align: center;
-  background: #fff;
+  background: #88ff81;
   border-radius: 3px;
   z-index: 4;
 
@@ -43,7 +42,6 @@ export const Box = styled.div`
 
   @media (max-width: 480px) {
     width: 35%;
-    background: #f4ab9f;
   }
 
   @media (max-width: 410px) {
@@ -52,6 +50,5 @@ export const Box = styled.div`
 `
 export const Body = styled.p`
   font-size: 2rem;
-  color: #8d3c2e;
-  padding: 1rem;
+  color: #ff125f;
 `
